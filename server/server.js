9673b16f2f7e24a868304ac7b86013eab86adbcf
Samarth Jadhav <samarth10jadhav@gmail.com>
2026-01-12 00:50:08 +0530

    Final production-ready server update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : 'https://inventory-management-dashboard-pi.vercel.app'
+}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
