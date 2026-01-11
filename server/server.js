fd24c90f2614ddd3d3114f55c4d9b54af5582b0e
Samarth Jadhav <samarth10jadhav@gmail.com>
2026-01-11 23:37:07 +0530

    final 2 commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // This allows the connection without a physical .pem certificate file
+  }
 });
 
 // Connect to Database
