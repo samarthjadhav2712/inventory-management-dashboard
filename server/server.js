22464f35f00d5a250f79bb091b27fadc7d6ab421
Samarth Jadhav <samarth10jadhav@gmail.com>
2026-01-12 00:58:53 +0530

    Final production-ready server update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin : 'https://inventory-management-dashboard-s2bi.vercel.app/'
+  origin : 'https://inventory-management-dashboard-s2bi.vercel.app'
 }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
