b5743a5094bdf1fced2517e6d8ef791550f6bb0e
Samarth Jadhav <samarth10jadhav@gmail.com>
2026-01-11 23:47:08 +0530

    final 3 commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DB_PORT || 5000;
 
 // Middleware
 app.use(cors());
